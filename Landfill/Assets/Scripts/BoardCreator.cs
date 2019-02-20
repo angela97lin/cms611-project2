e96608b14e9dd95dbe035f6b0ad2e0929952a8dd
Daniel Dangond <dangond@mit.edu>
2019-02-20 15:40:31 -0500

    Create connected grid

---
+++ Landfill/Assets/Scripts/BoardCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCreator : MonoBehaviour
+{
+    public int gridWidth = 7;
+    public int gridHeight = 14;
+    public float spriteSize = 1f;
+    public Transform boardCenterTransform;
+    public GridSpace gridSpacePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBoard(gridWidth, gridHeight);
+    }
+
+    void CreateBoard(int width, int height)
+    {
+        // Initialize empty board
+        List<List<GridSpace>> board = new List<List<GridSpace>>();
+        for (int i = 0; i < height; i++)
+        {
+            board.Add(new List<GridSpace>());
+        }
+
+        // Create board spaces
+        for (int yIndex = 0; yIndex < height; yIndex++)
+        {
+            for (int xIndex = 0; xIndex < width; xIndex++)
+            {
+                board[yIndex].Add(Instantiate(gridSpacePrefab, GridSpacePosition(xIndex, yIndex, width, height), Quaternion.identity)); // TODO: Instantiate in proper location
+            }
+        }
+
+        for (int yIndex = 0; yIndex < height; yIndex++)
+        {
+            for (int xIndex = 0; xIndex < width; xIndex++)
+            {
+                GridSpace currentSpace = board[yIndex][xIndex];
+                if (yIndex + 1 < height)
+                {
+                    currentSpace.SetDownSpace(board[yIndex + 1][xIndex]);
+                }
+                if (xIndex + 1 < width)
+                {
+                    currentSpace.SetRightSpace(board[yIndex][xIndex + 1]);
+                }
+            }
+        }
+        board[0][0].SetActive();
+    }
+
+    Vector2 GridSpacePosition(int xIndex, int yIndex, int width, int height)
+    {
+        return new Vector2(xIndex - (width / 2) + 0.5f, -1 * (yIndex - (height / 2) + 0.5f)) * spriteSize;
+    }
+}
---
+++ Landfill/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour
+{
+    GridSpace leftSpace = null;
+    GridSpace rightSpace = null;
+    GridSpace upSpace = null;
+    GridSpace downSpace = null;
+    bool isActive = false;
+    static float movedTime = 0f;
+
+    public void Update()
+    {
+        // NOTE: This is only for testing grid connectivity
+        //if (isActive)
+        //{
+        //    transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        //    if (Mathf.Abs(movedTime - Time.time) > 0.0001f)
+        //    {
+        //        if (Input.GetAxisRaw("Horizontal") > 0.5f && rightSpace != null)
+        //        {
+        //            rightSpace.isActive = true;
+        //            isActive = false;
+        //            movedTime = Time.time;
+        //        }
+        //        else if (Input.GetAxisRaw("Horizontal") < -0.5f && leftSpace != null)
+        //        {
+        //            leftSpace.isActive = true;
+        //            isActive = false;
+        //            movedTime = Time.time;
+        //        }
+        //        else if (Input.GetAxisRaw("Vertical") > 0.5f && upSpace != null)
+        //        {
+        //            upSpace.isActive = true;
+        //            isActive = false;
+        //            movedTime = Time.time;
+        //        }
+        //        else if (Input.GetAxisRaw("Vertical") < -0.5f && downSpace != null)
+        //        {
+        //            downSpace.isActive = true;
+        //            isActive = false;
+        //            movedTime = Time.time;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    transform.localScale = new Vector3(1,1,1);
+        //}
+    }
+
+    public void SetActive()
+    {
+        isActive = true;
+    }
+
+    public GridSpace GetLeftSpace()
+    {
+        return leftSpace;
+    }
+
+    public GridSpace GetRightSpace()
+    {
+        return rightSpace;
+    }
+
+    public GridSpace GetUpSpace()
+    {
+        return upSpace;
+    }
+
+    public GridSpace GetDownSpace()
+    {
+        return downSpace;
+    }
+
+    public void SetLeftSpace(GridSpace space)
+    {
+        leftSpace = space;
+        space.rightSpace = this;
+    }
+
+    public void SetRightSpace(GridSpace space)
+    {
+        rightSpace = space;
+        space.leftSpace = this;
+    }
+
+    public void SetUpSpace(GridSpace space)
+    {
+        upSpace = space;
+        space.downSpace = this;
+    }
+
+    public void SetDownSpace(GridSpace space)
+    {
+        downSpace = space;
+        space.upSpace = this;
+    }
+}
